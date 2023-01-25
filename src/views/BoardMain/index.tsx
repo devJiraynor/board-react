890f6a7d99e048b104b127f31900d899ab4b8781
devJiraynor <jiraynor@naver.com>
2023-01-25 22:09:49 +0900

    Chapter 12

---
+++ src/views/BoardMain/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function BoardMain() {
+  return (
+    <div>Board Main</div>
+  )
+}
--- src/views/layouts/MainLayout/index.tsx
+++ src/views/layouts/MainLayout/index.tsx
@@ -1,12 +1,38 @@
-import React from 'react'
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { useCookies } from 'react-cookie';
+import { useUserStore } from '../../../stores';
 import Authentication from '../../Authentication'
+import BoardMain from '../../BoardMain'
 import Navigation from '../../Navigation'
 
 export default function MainLayout() {
+
+  const [boardResponse, setBoardResponse] = useState<string>('');
+  const [cookies] = useCookies();
+  const { user } = useUserStore();
+
+  const getBoard = async (token: string) => {
+    const requsetOption = {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    };
+    
+    await axios.get('http://localhost:4000/api/board/', requsetOption).then((response) => {
+      setBoardResponse(response.data);
+    }).catch((error) => '');
+  }
+
+  useEffect(() => {
+    const token = cookies.token;
+    if (token) getBoard(token);
+  }, [user]);
+
   return (
     <>
         <Navigation />
-        <Authentication />
+        {boardResponse ? (<BoardMain />) : (<Authentication />)}
     </>
   )
 }
